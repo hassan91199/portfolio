9214589b99c12be0eb1f24261b4ff251b4776665
Muhammad Hassan Abbas <hassanabbas4745@gmail.com>
2022-08-18 19:36:43 +0500

    added contact form

--- index.js
+++ index.js
@@ -13,3 +13,22 @@ function expand(parentClassOfButton) {
     moreText.style.display = "inline";
   }
 }
+
+function showContactForm() {
+  var contactBtn = document.getElementById("contact-btn");
+  var contactForm = document.getElementById("contact-form");
+
+  contactBtn.classList.add("contact-btn-hide");
+  contactBtn.classList.remove("contact-btn-show");
+
+
+  contactForm.classList.add("contact-form-show");
+  contactForm.classList.remove("contact-form-hide");
+
+}
+
+function showThankYou() {
+  var thankYouText = document.getElementsByClassName("thank-you")[0];
+
+  thankYouText.style.visibility = "visible";
+}
